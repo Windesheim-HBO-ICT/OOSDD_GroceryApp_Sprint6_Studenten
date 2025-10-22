8cf003137e29b2dc5d6b00718f44ad9ca7ece06b
Ronald van de Velde <s1208278@student.windesheim.nl>
2025-10-22 19:35:49 +0200

    Category repository geupdate voor het gebruik van een database
    heb alle repository functies aangepast zodat er nu sql(ite) querys worden gebruikt

--- Grocery.Core.Data/Repositories/CategoryRepository.cs
+++ Grocery.Core.Data/Repositories/CategoryRepository.cs
@@ -1,29 +1,53 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Grocery.Core.Data.Repositories
 {
-    public class CategoryRepository : ICategoryRepository
+    public class CategoryRepository : DatabaseConnection, ICategoryRepository
     {
-        private readonly List<Category> categories;
+        private readonly List<Category> categories = [];
+
         public CategoryRepository()
         {
-            categories = [
-                new Category(1, "Groente"),
-                new Category(2, "Bakkerij"),
-                new Category(3, "Zuivel"),
-                new Category(4, "Conserven"),
-                new Category(5, "Ontbijt")];
+
+            //ISO 8601 format: date.ToString("o", CultureInfo.InvariantCulture)
+            CreateTable(@"CREATE TABLE IF NOT EXISTS Category (
+                            [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                            [Name] NVARCHAR(80) UNIQUE NOT NULL)");
+            List<string> insertQueries = [@"INSERT OR IGNORE INTO Category(Name) VALUES('Groente')",
+                                          @"INSERT OR IGNORE INTO Category(Name) VALUES('Bakkerij')",
+                                          @"INSERT OR IGNORE INTO Category(Name) VALUES('Zuivel')",
+                                          @"INSERT OR IGNORE INTO Category(Name) VALUES('Conserven')",
+                                          @"INSERT OR IGNORE INTO Category(Name) VALUES('Ontbijt')"];
+            InsertMultipleWithTransaction(insertQueries);
+            GetAll();
+        }
+        public List<Category> GetAll()
+        {
+            categories.Clear();
+            string selectQuery = "SELECT Id, Name FROM Category";
+            OpenConnection();
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    categories.Add(new(id, name));
+                }
+            }
+            CloseConnection();
+
+            return categories;
         }
 
         public Category? Get(int id)
         {
-            return categories.FirstOrDefault(c => c.Id == id);
-        }
-
-        public List<Category> GetAll()
-        {
-            return categories;
+            this.GetAll();
+            return categories.FirstOrDefault(p => p.Id == id);
         }
     }
 }
